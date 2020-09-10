1d7939eb7b4528a745eaa4df7a03f19a4d263a00
Jesse McCarville <pango3001@gmail.com>
2020-09-10 11:26:27 -0500

    added function to get current directory

--- mydu.c
+++ mydu.c
@@ -4,18 +4,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
+#include <string.h>
+
+
+
+#define MAX_ARGS 10
 //#include <unistd.h>
 
 
 //======== PROTOTYPES =========
-int depthfirstapply(char *path, int pathfun(char *path1));
+int depthfirstapply(char *path, int pathfun(char *path)),char *opts);
 int sizepathfun(char *path);
+char
 
 //=========== MAIN ============
 
 int main(int argc, char **argv){
 	int options;
-	while((options = getopt(argc, argv, "haB:bmcdNHLs")) != -1){
+	char selected_options[MAX_ARGS];
+	while((options = getopt(argc, argv, "haB:bmcd:HLs")) != -1){
 		switch(options){
 			case 'h':  
 				printf("Usage: mydu [-a] [-B M | -b | -m] [-c] [-d N] [-H] [-L] [-s] <dir1> <dir2> ...\n");
@@ -31,39 +38,62 @@ int main(int argc, char **argv){
 				printf("\t-s Display only a total for each argument\n");
 				return EXIT_SUCCESS;
 			case 'a':
+				strcat(selected_options, "a");
 				break;
 			case 'B':
+				strcat(selected_options, "B");
 				break;
 			case 'b':
+				strcat(selected_options, "b");
 				break;
 			case 'm':
-				break;  
+				strcat(selected_options, "m");
+				break;
 			case 'c':
+				strcat(selected_options, "c");
 				break;
 			case 'd':
+				strcat(selected_options, "d");
 				break;
 			case 'N':
+				strcat(selected_options, "N");	
 				break;
 			case 'H':
+				strcat(selected_options, "H");
 				break;
 			case 'L':
+				strcat(selected_options, "L");
 				break;
 			case 's':
+				strcat(selected_options, "s");
 				break;
-        }  
-    }  
-    
-int depthfirstapply(char *path, int pathfun(char *path1));
+		}
+	}
+	//printf("Options selected: %s \n",selected_options);
+	int total = depthfirstapply(char *path, int pathfun(char *path), char *opts);
 
-    return(0);
+	return(0);
 }
 
-
-int depthfirstapply(char *path, int pathfun(char *path1)){
-
-    return (0);
+int depthfirstapply(char* path, int pathfun(char *path), char *opts){
+	DIR *directory;
+	
 }
 
 int sizepathfun(char *path){
-    return(0);
+	return(0);
+}
+
+//returns a starting directory is given, if not given, current dir will be default
+char *check_dir(char * dir){
+	char directory[4096];
+	char cur_directory[2] = ".";
+	if(dir == NULL){
+		directory = cur_directory;
+	}
+	else {
+		directory = dir;
+	}
+
+	return (directory);
 }
